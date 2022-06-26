dd41e001cc92b1a44496490004650e126d912dc7
HenriqueNasciment0 <henrique@HENRIQUE-ACER.localdomain>
2022-06-26 20:52:30 -0300

    print e variáveis com Ruby

---
+++ hello.rb
@@ -0,0 +1,8 @@
+puts "hello word" # printar na tela, também funciona com
+# palavra reservada, print
+
+nome = "henrique"; # variável
+sobrenome = "nascimento"; # variável - funcionou somente com 
+# aspas duplas
+
+puts "Meu nome é #{nome} #{sobrenome}"
