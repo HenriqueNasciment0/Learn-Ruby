0355f12985851146d884be39457eff0982dcc27f
HenriqueNasciment0 <you@example.com>
2022-06-27 19:25:59 -0300

    inicio de testes boleanos

---
+++ dia02/boleanos.rb
@@ -0,0 +1,7 @@
+quatro = 4
+mult = 2 * 2
+
+puts "#{quatro}" === "#{mult}" #true
+puts "#{quatro}" < "#{mult}" #false
+puts "#{quatro}" != "#{mult}" #false
+puts "#{quatro}" <=> "#{mult}" #0 (comparação igual)
