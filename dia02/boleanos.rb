64300a1cffbf76ea2da520493db725fd025322ab
HenriqueNasciment0 <you@example.com>
2022-06-27 19:39:40 -0300

    if else com ruby

--- dia02/boleanos.rb
+++ dia02/boleanos.rb
@@ -5,3 +5,11 @@ puts "#{quatro}" === "#{mult}" #true
 puts "#{quatro}" < "#{mult}" #false
 puts "#{quatro}" != "#{mult}" #false
 puts "#{quatro}" <=> "#{mult}" #0 (comparação igual)
+
+x = 11
+if quatro == 4
+  puts "é igual"
+else
+  puts "não é igual"
+end
+
