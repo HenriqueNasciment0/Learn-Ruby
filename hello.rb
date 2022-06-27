3872003bcfb3707cc5037b4babae7bcacd1a9e12
HenriqueNasciment0 <you@example.com>
2022-06-26 21:07:49 -0300

    interagindo no console com Ruby

--- hello.rb
+++ hello.rb
@@ -2,7 +2,15 @@ puts "hello word" # printar na tela, também funciona com
 # palavra reservada, print
 
 nome = "henrique"; # variável
-sobrenome = "nascimento"; # variável - funcionou somente com 
+sobrenome = "nascimento"; # variável - funcionou somente com
 # aspas duplas
 
 puts "Meu nome é #{nome} #{sobrenome}"
+
+# -------------------------
+
+puts "Qual seu nome bb?"
+nome = gets.chomp # recebe texto no console
+puts "E sua idade bb?"
+idade = gets.chomp.to_i # recebe texto e converte para número inteiro
+puts "#{nome} tem #{idade} aninhos!"
